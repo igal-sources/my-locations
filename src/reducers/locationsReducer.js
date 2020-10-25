cf81182609f0d0eb8ccc639548fdeb2b2a4c5997
Igal Leibovich <igal2025@gmail.com>
2020-10-25 06:52:18 +0200

    handle Delete location

--- src/reducers/locationsReducer.js
+++ src/reducers/locationsReducer.js
@@ -43,7 +43,6 @@ export const locationsReducer = (state = defaultState, action) => {
       };
     case types.REMOVE_LOCATION:
       return {
-        ...state,
         ...state.locations.filter((c) => c.locationId !== action.location.locationId),
       };
     default:
